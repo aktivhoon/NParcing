0e60b7b3f35365edd9665f6ab3845232fc8e34b7
unknown <diti94@naver.com>
2022-11-03 13:12:59 +0900

    UI 변경 준비 작업

--- parsing.py
+++ parsing.py
@@ -3,6 +3,11 @@ from openpyxl import Workbook
 from openpyxl.styles import Alignment, Border, Side, DEFAULT_FONT
 from datetime import datetime, timedelta
 
+def what_day_is_it(date):
+    days = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일']
+    day = date.weekday()
+    return days[day]
+
 def set_border(ws, cell_range):
     thin = Side(border_style="thin", color="000000")
     for row in ws[cell_range]:
@@ -138,12 +143,14 @@ def generate_excel(input1, input2,_61_empty,_61_man,_61_woman,_62_empty,_62_man,
 
     write_wb = Workbook()
     write_ws = write_wb.active
-    start_62 = max(1,len(adm_61))+max(1,len(dc_61))+3
-    start_37 = max(1,len(adm_61))+max(1,len(adm_62))+max(1,len(dc_61))+max(1,len(dc_62))+5
-    start_121 = max(1,len(adm_61))+max(1,len(adm_62))+max(1,len(adm_37))+max(1,len(dc_61))+max(1,len(dc_62))+max(1,len(dc_37))+6
-    start_opd = max(1,len(adm_61))+max(1,len(adm_62))+max(1,len(adm_37))+max(1, len(adm_121))+max(1,len(dc_61))+max(1,len(dc_62))+max(1,len(dc_37))+max(1,len(dc_121))+7
+    
+    start_61 = 2 #will be edited
+    start_62 = max(1,len(adm_61))+max(1,len(dc_61))+2+start_61
+    start_37 = start_62+max(1,len(adm_62))+max(1,len(dc_62))+2
+    start_121 = start_37+max(1,len(adm_37))+max(1,len(dc_37))+1
+    start_opd = start_121+max(1, len(adm_121))+max(1,len(dc_121))+1
 
-    write_adm_dc(write_ws, adm_61, dc_61, _61_empty,_61_man,_61_woman, start_row=1, ward_name="61병동")
+    write_adm_dc(write_ws, adm_61, dc_61, _61_empty,_61_man,_61_woman, start_row=start_61, ward_name="61병동")
     write_adm_dc(write_ws, adm_62, dc_62, _62_empty,_62_man,_62_woman, start_row=start_62, ward_name="62병동")
     write_adm_dc(write_ws, adm_37, dc_37, start_row=start_37, ward_name="낮병원")
     write_adm_dc(write_ws, adm_121, dc_121, start_row=start_121, ward_name="특실")
@@ -154,6 +161,7 @@ def generate_excel(input1, input2,_61_empty,_61_man,_61_woman,_62_empty,_62_man,
     write_ws.cell(start_opd, 2, "특이사항 없음")
     currentCell = write_ws.cell(start_opd, 2)
     currentCell.alignment = Alignment(horizontal='center', vertical='center')
+    write_ws.row_dimensions[1].height = 16.5
     write_ws.column_dimensions['A'].width = 8
     write_ws.column_dimensions['B'].width = 9.38
     write_ws.column_dimensions['C'].width = 11.13
@@ -162,16 +170,18 @@ def generate_excel(input1, input2,_61_empty,_61_man,_61_woman,_62_empty,_62_man,
     write_ws.column_dimensions['F'].width = 51.38
     write_ws.column_dimensions['G'].width = 41
 
-    
-
-    set_border(write_ws, 'A1:G{}'.format(start_opd)) 
-    set_thick_border(write_ws, 'A1:G{}'.format(start_62-1))
+    set_border(write_ws, 'A{}:G{}'.format(start_61,start_opd)) 
+    set_thick_border(write_ws, 'A{}:G{}'.format(start_61, start_62-1))
     set_thick_border(write_ws, 'A{}:G{}'.format(start_62, start_37-1))
     set_thick_border(write_ws, 'A{}:G{}'.format(start_37, start_121-1))
     set_thick_border(write_ws, 'A{}:G{}'.format(start_121, start_opd-1))
     set_thick_border(write_ws, 'A{}:G{}'.format(start_opd, start_opd))
-
+    
+    write_ws.merge_cells(start_row=1, start_column=1, end_row=1, end_column=7)
     yesterday = datetime.today() - timedelta(1)
+    write_ws.cell(1, 1, str(yesterday.year)+"년 "+str(yesterday.month)+"월 "+str(yesterday.day)+"일 "+what_day_is_it(yesterday)+" 당직보고")
+    currentCell = write_ws.cell(1, 1)
+    currentCell.alignment = Alignment(vertical='center')
     filename = "당직보고_"+yesterday.strftime("%Y%m%d")+".xlsx"
     write_wb.save(filename)
     return filename
